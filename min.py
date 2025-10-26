af8fffc927fc5aac46c98293c05d178294d97222
Aiperi008 <mamatovaaiperi@gmail.com>
2025-10-26 09:37:36 +0600

    white comething

--- min.py
+++ min.py
@@ -1,2 +1,6 @@
 print('Hello World from main.py')
 print('Hello World from main.py')
+
+
+for i in range(10):
+    print(i)
---
+++ test.py
@@ -0,0 +1,4 @@
+input('write comething')
+for i in range(10):
+    print(i)
+    input('write comething')
